bc0760a80fcdaab20f83390eeb3f932a077ac19f
Rosemery <rosemarybeck5@gmail.com>
2024-07-19 09:41:27 +0300

    asd

--- src/components/admin-panel/user-nav.tsx
+++ src/components/admin-panel/user-nav.tsx
@@ -57,15 +57,15 @@ export function UserNav() {
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem className="hover:cursor-pointer" asChild>
-            <Link href="/dashboard" className="flex items-center">
+            <Link href="/" className="flex items-center">
               <LayoutGrid className="w-4 h-4 mr-3 text-muted-foreground" />
               Dashboard
             </Link>
           </DropdownMenuItem>
           <DropdownMenuItem className="hover:cursor-pointer" asChild>
-            <Link href="/account" className="flex items-center">
+            <Link href="/settings" className="flex items-center">
               <User className="w-4 h-4 mr-3 text-muted-foreground" />
-              Account
+              Settings
             </Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
