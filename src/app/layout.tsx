28ff847478baee428c6c26f41b0bac3cb62ded2b
Rosemery <rosemarybeck5@gmail.com>
2024-09-26 14:26:15 +0300

    add logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <link rel="icon" href="logo.svg" sizes="any" type="image/svg+xml" />
       <body className={GeistSans.className}>
         <QueryProvider>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
--- src/components/admin-panel/sidebar.tsx
+++ src/components/admin-panel/sidebar.tsx
@@ -9,43 +9,77 @@ import { useSidebarToggle } from "@/hooks/use-sidebar-toggle";
 import { SidebarToggle } from "@/components/admin-panel/sidebar-toggle";
 
 export function Sidebar() {
-	const sidebar = useStore(useSidebarToggle, (state) => state);
+  const sidebar = useStore(useSidebarToggle, (state) => state);
 
-	if (!sidebar) return null;
+  if (!sidebar) return null;
 
-	return (
-		<aside
-			className={cn(
-				"fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
-				sidebar?.isOpen === false ? "w-[90px]" : "w-72"
-			)}
-		>
-			<SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
-			<div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
-				<Button
-					className={cn(
-						"transition-transform ease-in-out duration-300 mb-1",
-						sidebar?.isOpen === false ? "translate-x-1" : "translate-x-0"
-					)}
-					variant="link"
-					asChild
-				>
-					<Link href="/" className="flex items-center gap-2">
-						<PanelsTopLeft className="w-6 h-6 mr-1" />
-						<h1
-							className={cn(
-								"font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300",
-								sidebar?.isOpen === false
-									? "-translate-x-96 opacity-0 hidden"
-									: "translate-x-0 opacity-100"
-							)}
-						>
-							Flex AI
-						</h1>
-					</Link>
-				</Button>
-				<Menu isOpen={sidebar?.isOpen} />
-			</div>
-		</aside>
-	);
+  return (
+    <aside
+      className={cn(
+        "fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300",
+        sidebar?.isOpen === false ? "w-[90px]" : "w-72"
+      )}
+    >
+      <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
+      <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
+        <Button
+          className={cn(
+            "transition-transform ease-in-out duration-300 mb-1",
+            sidebar?.isOpen === false ? "translate-x-1" : "translate-x-0"
+          )}
+          variant="link"
+          asChild
+        >
+          <Link href="/" className="flex items-center gap-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 38 38">>
+              <defs>
+                <linearGradient
+                  id="b"
+                  x1="87.629%"
+                  x2="9.97%"
+                  y1="28.473%"
+                  y2="86.882%"
+                >
+                  <stop offset="0%" stop-color="#F1F5F9" stop-opacity="0" />
+                  <stop offset="100%" stop-color="#F1F5F9" />
+                </linearGradient>
+                <filter
+                  id="a"
+                  width="141.4%"
+                  height="141.4%"
+                  x="-20.7%"
+                  y="-20.7%"
+                  filterUnits="objectBoundingBox"
+                >
+                  <feGaussianBlur in="SourceGraphic" stdDeviation="2" />
+                </filter>
+              </defs>
+              <g fill="none" fill-rule="nonzero">
+                <path
+                  fill="#A855F7"
+                  d="M19 33.5c-8.008 0-14.5-6.492-14.5-14.5S10.992 4.5 19 4.5 33.5 10.992 33.5 19 27.008 33.5 19 33.5Zm0-5a9.5 9.5 0 1 0 0-19 9.5 9.5 0 0 0 0 19Z"
+                  filter="url(#a)"
+                />
+                <path
+                  fill="url(#b)"
+                  d="M19 33.5c-8.008 0-14.5-6.492-14.5-14.5S10.992 4.5 19 4.5 33.5 10.992 33.5 19 27.008 33.5 19 33.5Zm0-5a9.5 9.5 0 1 0 0-19 9.5 9.5 0 0 0 0 19Z"
+                />
+              </g>
+            </svg>
+            <h1
+              className={cn(
+                "font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300",
+                sidebar?.isOpen === false
+                  ? "-translate-x-96 opacity-0 hidden"
+                  : "translate-x-0 opacity-100"
+              )}
+            >
+              Flex AI
+            </h1>
+          </Link>
+        </Button>
+        <Menu isOpen={sidebar?.isOpen} />
+      </div>
+    </aside>
+  );
 }
