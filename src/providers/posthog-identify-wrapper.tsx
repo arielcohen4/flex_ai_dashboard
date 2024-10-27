7c420032a41be2a75edeefae04b1e97ed5a16e17
rosemary back <rosemarybeck5@gmail.com>
2024-10-27 22:08:12 +0200

    update

--- src/providers/posthog-identify-wrapper.tsx
+++ src/providers/posthog-identify-wrapper.tsx
@@ -3,9 +3,26 @@
 import { useEffect, type PropsWithChildren } from "react";
 import posthog from "posthog-js";
 import useUser from "@/app/hook/useUser";
+import { useSearchParams } from "next/navigation";
 
 export function PostHogIdentifyWrapper({ children }: PropsWithChildren) {
   const { data: user } = useUser();
+
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const signin = searchParams.get("signin");
+    if (signin === "true") {
+      posthog.capture("user_signed_in", {
+        source: "query_parameter",
+      });
+
+      // Remove the signin parameter from the URL
+      const url = new URL(window.location.href);
+      url.searchParams.delete("signin");
+      window.history.replaceState({}, "", url.toString());
+    }
+  }, [searchParams]);
 
   useEffect(() => {
     if (user?.id) {
